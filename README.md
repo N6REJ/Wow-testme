a80b10b566164f693e832b6dbadacd4005992453
Bear <programming@hallhome.us>
2021-02-20 05:05:01 -0600

    added core files
    basic file structure

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Wow-testme
-Addon Skeleton for wow shadowlands or greater.
+# AdiBags - Shadowlands Crafting
+Wow AdiBags - Shadowlands Crafting addons for AdiBags
+
+The purpose is to be a single category filter for AdiBags
+so that those that only need select categories can just 
+use those addons.
+
+### REQUIRES AdiBags for AdiBags - Shadowlands Crafting to function. ###
---
+++ testme/database.lua
@@ -0,0 +1,32 @@
+--[[
+Project: Testme
+Author: N6REJ
+Character: Bearesquishy
+Server: Dalaran
+License: MIT
+Purpose: database
+--]]
+
+
+local ADDON_NAME, ADDON_TABLE, addon = ...
+
+-- Create addon table
+N = { }
+ADDON_TABLE.N = N
+
+-- Label to use
+N["FilterTitle"] = "Tailoring"
+
+-- Database of items
+N["database"] = {
+	172439,	-- Enchanted Lightless Silk
+	173202,	-- Shrouded Cloth
+	173204,	-- Lightless Silk
+	177062,	-- Penumbra Thread
+	182028,	-- Bleakthread
+	182050,	-- Prideweave Cloth
+	182052,	-- Thread of pride
+	182103,	-- Gossamer Cloth
+	338270,	-- Ardensilk Cloth
+	338277,	-- Bleakcloth
+}
